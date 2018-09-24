e59e723fb5cfddaf9ef1af78fc8097ce52d8fd9a
kirya <cloudkserg11@gmail.com>
2018-09-24 17:09:40 +0300

    https://chronobank.atlassian.net/browse/MD-688 - parseInt to CHECK_SYSTEM

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ const config = {
     exchange: process.env.SYSTEM_RABBIT_EXCHANGE || 'internal',
     serviceName: process.env.SYSTEM_RABBIT_SERVICE_NAME || 'system' 
   },
-  checkSystem: process.env.CHECK_SYSTEM || true,
+  checkSystem: process.env.CHECK_SYSTEM ? parseInt(process.env.CHECK_SYSTEM) : true,
   smartContracts: {
     networkId: process.env.SMART_CONTRACTS_NETWORK_ID || '4',
     path: process.env.SMART_CONTRACTS_PATH || path.join(__dirname, '../node_modules/chronobank-smart-contracts/build/contracts'),
